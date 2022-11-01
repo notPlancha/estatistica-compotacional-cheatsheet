a0f8180d09f7e66c7548b52053ec4822eee58010
notPlancha <andre.plancha@hotmail.com>
2022-11-01 22:41:17 +0000

    até capitulo IV feito ish

---
+++ unusedChecker/removeUnsedPackages.py
@@ -0,0 +1,3 @@
+# TODO make pdf with the tectonic, and remove packages 1 by one to check if they yield an error or make a different pdf (with hash checker)
+# then it returns not used packages
+# good place to start https://github.com/TarasKuzyo/LaTeXpkges/blob/master/latexpkges3.py
